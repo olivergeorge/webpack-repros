4d7fd5d5b7e58ee4d512b81e635db64dca9cbe3f
Oliver George <oliver@condense.com.au>
2020-05-03 20:46:56 +1000

    @aws-amplify.ui-react test

--- src/hello_bundler/core.cljs
+++ src/hello_bundler/core.cljs
@@ -1,11 +1,19 @@
 (ns hello-bundler.core
   (:require [react]
-            ["aws-amplify" :as aws]))
+            ["aws-amplify" :as aws]
+            ["@aws-amplify/ui-react" :as ui-react]))
 
-(def m aws)
+(def m1 aws)
 
 (.log js/console react/Component)
-(.log js/console "aws" m)
-(.log js/console "aws type" (type m))
-(.log js/console "aws keys" (goog.object/getKeys m))
+
+(.log js/console "aws" m1)
+(.log js/console "aws type" (type m1))
+(.log js/console "aws keys" (goog.object/getKeys m1))
+
+(def m2 ui-react)
+
+(.log js/console "ui-react" m2)
+(.log js/console "ui-react type" (type m2))
+(.log js/console "ui-react keys" (goog.object/getKeys m2))
 

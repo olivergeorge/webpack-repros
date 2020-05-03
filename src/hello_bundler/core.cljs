b1bc46c66e1634c9dbf3c8cd03e7d488953255ce
Oliver George <oliver@condense.com.au>
2020-05-03 20:44:21 +1000

    aws-amplify test

--- src/hello_bundler/core.cljs
+++ src/hello_bundler/core.cljs
@@ -1,4 +1,11 @@
 (ns hello-bundler.core
-  (:require [react]))
+  (:require [react]
+            ["aws-amplify" :as aws]))
+
+(def m aws)
 
 (.log js/console react/Component)
+(.log js/console "aws" m)
+(.log js/console "aws type" (type m))
+(.log js/console "aws keys" (goog.object/getKeys m))
+

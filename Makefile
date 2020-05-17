412e1c067aa276d6ed00218519df60cefe211495
Oliver George <oliver@condense.com.au>
2020-05-17 22:15:07 +1000

    Overriding a Foreign Library

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
 
+clean:
+	rm -rf out
+
+setup:
+	yarn
+
 repl:
 	clj -m cljs.main -co build.edn -v -c -r
 
--- src/hello_bundler/core.cljs
+++ src/hello_bundler/core.cljs
@@ -1,4 +1,12 @@
 (ns hello-bundler.core
-  (:require [react]))
+  (:require [goog.dom :as gdom]
+            [reagent.dom :as dom]))
 
-(.log js/console react/Component)
+(defn simple-component []
+  [:div
+   [:p "I am a component!"]
+   [:p.someclass
+    "I have " [:strong "bold"]
+    [:span {:style {:color "red"}} " and red "] "text."]])
+
+(dom/render [simple-component] (gdom/getElement "app"))

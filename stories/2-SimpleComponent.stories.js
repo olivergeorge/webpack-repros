aa9cf52266f36e5057b9a3b96a33a3a113b19ec9
Oliver George <oliver@condense.com.au>
2020-05-18 10:50:23 +1000

    add missing story file

---
+++ stories/2-SimpleComponent.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../out/index.js';
+
+export default {
+  title: 'SimpleComponent',
+  component: SimpleComponent,
+};
+
+export const SimpleComponent = () => <hello_bundler.core.SimpleComponent />;
+
